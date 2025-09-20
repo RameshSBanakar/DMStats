ed2469fe7ae449f838eb461cf59c37f7631169b5
ramesh banakar <ramesh.s.b@campusuvce.in>
2025-09-20 22:33:23 +0530

    system info module code changing

--- Controllers/HistoryController.js
+++ Controllers/HistoryController.js
@@ -1,58 +1,83 @@
 const History = require("../Models/HistoryModel");
-const si = require("systeminformation");
+
+let si;
+try {
+  si = require("systeminformation");
+} catch (err) {
+  console.warn(
+    "systeminformation module not found. Disk/system info functionality will be disabled."
+  );
+  si = null; // fallback so app doesn't crash
+}
+
 exports.addHistory = async (req, res) => {
-  const history = new History({
-    name: req.body.name,
-    user: req.user._id,
-  });
-  await history.save();
-  res.json({
-    status: true,
-    data: history,
-  });
+  try {
+    const history = new History({
+      name: req.body.name,
+      user: req.user._id,
+    });
+    await history.save();
+    res.json({
+      status: true,
+      data: history,
+    });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
 
 exports.removeHistory = async (req, res) => {
-  const deleteHistory = await History.findOneAndDelete({ _id: req.body._id });
-  if (deleteHistory) {
-    res.json({
-      status: true,
-      message: "Deleted",
-    });
-  } else {
-    res.json({ status: false, message: "not able to delete" });
+  try {
+    const deleteHistory = await History.findOneAndDelete({ _id: req.body._id });
+    if (deleteHistory) {
+      res.json({ status: true, message: "Deleted" });
+    } else {
+      res.json({ status: false, message: "Not able to delete" });
+    }
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
   }
-}
-  
+};
 
 exports.getHistory = async (req, res) => {
-  const history = await History.find({ user: req.user._id }).sort({
-    lastDate: -1,
-  });
-  res.json({
-    status: true,
-    data: history,
-  });
+  try {
+    const history = await History.find({ user: req.user._id }).sort({
+      lastDate: -1,
+    });
+    res.json({ status: true, data: history });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
-exports.systeminfo =async (req,res) => {
-  const { userAgent, screenSize } = req.body;
-  const diskInfo = await si.fsSize();
-  console.log((diskInfo[0].size)/Math.pow(1024,3));
-  console.log(diskInfo[0].used / Math.pow(1024, 3));
-  console.log(diskInfo[0].available / Math.pow(1024, 3));
-  const modifiedDiskInfo = diskInfo.map((disk) => ({
-    ...disk,
-    sizeGB: (disk.size / (1024 * 1024 * 1024)).toFixed(2), // Convert size to GB
-    usedGB: (disk.used / (1024 * 1024 * 1024)).toFixed(2), // Convert used space to GB
-    availableGB: (disk.available / (1024 * 1024 * 1024)).toFixed(2), // Convert available space to GB
-  }));
 
-  const systemInfo = {
-    userAgent,
-    screenSize,
-    modifiedDiskInfo,
-    // Add more system information as needed
-  };
-  console.log("Received client info:", systemInfo);
-  res.json(systemInfo);
+exports.systeminfo = async (req, res) => {
+  try {
+    const { userAgent, screenSize } = req.body;
+
+    let modifiedDiskInfo = [];
+    if (si) {
+      const diskInfo = await si.fsSize();
+      modifiedDiskInfo = diskInfo.map((disk) => ({
+        ...disk,
+        sizeGB: (disk.size / (1024 * 1024 * 1024)).toFixed(2),
+        usedGB: (disk.used / (1024 * 1024 * 1024)).toFixed(2),
+        availableGB: (disk.available / (1024 * 1024 * 1024)).toFixed(2),
+      }));
+    } else {
+      console.warn(
+        "Skipping disk info: systeminformation module not available."
+      );
+    }
+
+    const systemInfo = {
+      userAgent,
+      screenSize,
+      modifiedDiskInfo,
+    };
+
+    console.log("Received client info:", systemInfo);
+    res.json(systemInfo);
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
